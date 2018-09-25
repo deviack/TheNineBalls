3ca36824216750cf254c7cfd21279097799700bd
Deviack <deviacks@gmail.com>
2018-09-25 10:26:43 -0500

    Ya identifica el número mayor. Falta condicionar tipo de dato introducido (int)

---
+++ src/metodos/Metodos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.To change this template file, choose Tools | Templates, and open the template in the editor.
+ */
+package metodos;
+
+/**
+ *
+ * @author Citla
+ */
+public class Metodos {
+    
+    //Creamos el método sumar
+    public int suma(int a,int b,int c){
+        return a+b+c;
+    }
+    
+}
